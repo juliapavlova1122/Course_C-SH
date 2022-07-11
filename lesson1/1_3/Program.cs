ab1e4f9e204a34ede9754c9549fec1c03bc4dcd0
Julia Pavlova <jul11112@yandex.ru>
2022-07-11 15:06:28 +0300

    1_4

---
+++ lesson1/1_3/Program.cs
@@ -0,0 +1,21 @@
+﻿// 3 задача
+Console.WriteLine("Write a number: ");
+int n = int.Parse(Console.ReadLine());
+
+int i = -n;
+
+while (i <= n)
+{
+    Console.WriteLine(i);
+    i++;
+}
+
+Console.WriteLine("Write a number a: ");
+int a = int.Parse(Console.ReadLine());
+// if (a<0)
+int i=-a;
+while (i<=a)
+{
+    Console.WriteLine(i);
+    i++;
+}
---
+++ lesson1/1_4/Program.cs
@@ -0,0 +1,10 @@
+﻿// 4 задача
+int a = int.Parse(Console.ReadLine());
+int i = 0;
+int temp = 0;
+while (i < a * 2 + 1)
+{
+ Console.WriteLine(-a + temp);
+ i++;
+ temp++;
+}
