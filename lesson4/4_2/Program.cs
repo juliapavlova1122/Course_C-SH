ba397b6d6c3e36f2396d1e971b87b8055f8cf9f2
Julia Pavlova <jul11112@yandex.ru>
2022-08-02 11:07:53 +0300

    4_2

---
+++ lesson4/4_2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 2: Напишите программу, которая
+// принимает на вход число N и выдаёт
+// произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+int Factorial (int num)
+{
+    int AllSum = 1;
+    for (int i = 1; i <= num; i++)
+    AllSum = AllSum*i;
+    return AllSum;
+}
+
+Console.WriteLine(Factorial(4));
+
