577293b9583cfc2da68034c54d0a4f713ce2817b
Julia Pavlova <jul11112@yandex.ru>
2022-08-09 10:57:36 +0300

    6_2

---
+++ lesson6/6_2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Программа, преобразующая десятичное число в двоичное
+
+string ChangeNum (int num)
+{
+    string result = "";
+    while (num > 0)
+    {
+        result = num%2 + result;
+        num /= 2;
+    }
+    return result;
+}
+
+
+Console.WriteLine ($"12 -> {ChangeNum (12)}");
