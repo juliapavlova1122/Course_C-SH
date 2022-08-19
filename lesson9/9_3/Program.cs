ebc68817c9b7c845d1ff7d4c78ad2618ae6d5d5f
Julia Pavlova <jul11112@yandex.ru>
2022-08-19 11:12:48 +0300

    9_3

---
+++ lesson9/9_3/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 3: Напишите программу, которая 
+// будет принимать на вход число и
+// возвращать сумму его цифр. Использовать рекурсию.
+// 453 -> 12
+// 45 -> 9
+
+// Console.WriteLine ("Введите число: ");
+// int a = int.Parse (Console.ReadLine());
+
+// int sum = 0;
+// while (a > 0)
+// {
+//     sum+= a%10;
+//     a/= 10;
+// }
+// Console.WriteLine (sum);
+
+int sumnums (int sum)
+{
+    if (sum == 0) return 0;
+    return sumnums(sum/10) + sum% 10;
+}
+
+
+Console.WriteLine (sumnums (123));
